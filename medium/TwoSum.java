2640e77c574d43d3fe18d1b7307af34f93883aa7
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-27 23:03:04 +0800

    TwoSum

--- medium/TwoSum.java
+++ medium/TwoSum.java
@@ -2,26 +2,23 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The trick is to ignore earlier occurrences of duplicate values.
+ */
 public class TwoSum {
 
     public static Integer[] twoSum(int[] numbers, int target) {
     	Integer[] indices = new Integer[2];
     	
-    	// put all values-indices pairs into the map, overwriting earlier indices
     	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    	for (int i = 0; i < numbers.length; i++) {
-    		map.put(numbers[i], i);
-    	}
-    	
-    	// search
     	Integer temp;
     	for (int i = 0; i < numbers.length; i++) {
-    		temp = map.get(target - numbers[i]) ;
-    		if (temp != null && temp != i) {
-    			indices[0] = i + 1;
-    			indices[1] = temp + 1;
-    			
-    			return indices;
+    		temp = map.get(target - numbers[i]);
+    		if (temp != null) {
+    			indices[0] = temp + 1;
+				indices[1] = i + 1;
+    		} else {
+    			map.put(numbers[i], i);
     		}
     	}
     	
