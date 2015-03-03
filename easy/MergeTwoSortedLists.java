4e4d11c24002d58de19f4dda15ffb7d898005014
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-03 17:48:14 +0800

    MergeTwoSortedLists

--- easy/MergeTwoSortedLists.java
+++ easy/MergeTwoSortedLists.java
@@ -1,61 +1,24 @@
 
 /**
- * Very very tricky.
+ * Recursive version is simpler, less tricky.
  */
 public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    	ListNode node = null;
-    	ListNode head = null;
-    	
-    	// set initial
-    	if (l1 != null && l2 != null) {
-    		if (l1.val < l2.val) {
-				head = l1;
-	    		node = l1;
-	    		l1 = l1.next;
-    		} else {
-				head = l2;
-	    		node = l2;
-	    		l2 = l2.next;
-    		}
-    	} else if (l1 != null) {
-    		node = l1;
-			head = l1;
-    		l1 = l1.next;
-    	} else if (l2 != null) {
-    		node = l2;
-			head = l2;
-    		l2 = l2.next;
+    	if (l1 == null) {
+    		return l2;
+    	}
+    	if (l2 == null) {
+    		return l1;
     	}
     	
-    	// merge both
-		while (l1 != null && l2 != null) {
-			if (l1.val < l2.val) {
-				node.next = l1;
-				l1 = l1.next;
-				node = node.next;
-			} else {
-				node.next = l2;
-				l2 = l2.next;
-				node = node.next;
-			}
-		}
-		
-		
-		while (l1 != null) {
-			node.next = l1;
-			l1 = l1.next;
-			node = node.next;
-		}
-		
-		while (l2 != null) {
-			node.next = l2;
-			l2 = l2.next;
-			node = node.next;
-		}
-    	
-    	return head;
+    	if (l1.val < l2.val) {
+    		l1.next = mergeTwoLists(l1.next, l2);
+    		return l1;
+    	} else {
+    		l2.next = mergeTwoLists(l1, l2.next);
+    		return l2;
+    	}
     }
     
 	public static void main(String[] args) {
