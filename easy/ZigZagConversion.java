8dee6d1c7e74c940be7d482a6fc7f059af35815e
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-03 19:17:57 +0800

    ZigZagConversion

---
+++ easy/ZigZagConversion.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ZigZagConversion {
+
+    public String convert(String s, int nRows) {
+    	String converted = "";
+    	int midRow = (nRows-1)/2;
+    	
+    	// initialize rows
+    	List<StringBuilder> rows = new ArrayList<StringBuilder>();
+    	for (int i = 0; i < nRows; i++) {
+    		rows.add(new StringBuilder());
+    	}
+    	
+    	int r = 0;
+    	boolean secondColumn = false;
+    	for (int i = 0; i < s.length(); i++) {
+    		if (r == nRows) {
+    			secondColumn = true;
+    		}
+    		if (secondColumn) {
+    			rows.get(midRow).append(s.charAt(i));
+    			r = 0;
+    			secondColumn = false;
+    		} else {
+    			rows.get(r).append(s.charAt(i));
+    			r++;
+    		}
+    	}
+    	
+    	// summarize
+    	for (int i = 0; i < nRows; i++) {
+    		converted += rows.get(i).toString();
+    	}
+    	
+    	return converted;
+    }
+    
+	public static void main(String[] args) {
+		ZigZagConversion conversion = new ZigZagConversion();
+		System.out.println(conversion.convert("PAYPALISHIRING", 3)); // PAHNAPLSIIGYIR
+		System.out.println(conversion.convert("ABCD", 2)); // ACBD
+	}
+
+}
