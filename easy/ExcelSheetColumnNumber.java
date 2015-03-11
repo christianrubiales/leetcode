d67d14a72586ae16a05ae7b491bc2652e5ec7c6a
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-12 00:31:40 +0800

    ExcelSheetColumnNumber

---
+++ easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,25 @@
+
+public class ExcelSheetColumnNumber {
+
+	public static int titleToNumber(String s) {
+		int n = 0;
+		int b = (int) 'A' - 1;
+		
+		for (int i = 0; i < s.length(); i++) {
+			n += ((s.charAt(i) - b) * Math.pow(26, (s.length() - i - 1)));
+		}
+		
+		return n;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(titleToNumber("A"));//1
+		System.out.println(titleToNumber("B"));//2
+		System.out.println(titleToNumber("C"));//3
+		System.out.println(titleToNumber("Z"));//26
+		System.out.println(titleToNumber("AA"));//27
+		System.out.println(titleToNumber("AB"));//28
+		System.out.println(titleToNumber("BA"));//53
+	}
+
+}
