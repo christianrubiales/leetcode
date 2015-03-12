82dcabc5c343f22afb57fe52101299e3e1e2b2fa
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-12 23:34:28 +0800

    IntersectionOfTwoLinkedLists

--- easy/IntersectionOfTwoLinkedLists.java
+++ easy/IntersectionOfTwoLinkedLists.java
@@ -1,47 +1,52 @@
 
 /**
+ * Determine the lengths, start comparing from length of the shorter. 
  * Reversing does not work because it modifies the structures.
  */
 public class IntersectionOfTwoLinkedLists {
 
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA != null && headB != null) {
-            ListNode a = reverse(headA);
-            ListNode b = reverse(headB);
-            ListNode prev = null;
-            
-            while (a != null && b != null && a.val == b.val) {
-            	prev = a;
-            	a = a.next;
-            	b = b.next;
-            }
-            
-            headA = reverse(a);
-            headB = reverse(b);
-            
-            return prev;
+        	int l1 = length(headA);
+        	int l2 = length(headB);
+        	ListNode a = headA;
+        	ListNode b = headB;
+        	ListNode c = l1 > l2 ? a : b;
+        	
+        	// advance the pointer of the longer list
+        	int diff = l1 > l2 ? l1 - l2 : l2 - l1;
+    		for (int i = 0; i < diff; i++) {
+    			c = c.next;
+    		}
+    		if (l1 > l2) {
+    			a = c;
+    		} else {
+    			b = c;
+    		}
+        	
+    		for (int i = 0; i < Math.max(l1, l2) - diff; i++) {
+    			if (a.val == b.val) {
+    				return a;
+    			}
+    			a = a.next;
+    			b = b.next;
+    		}
+    		
         }
         
         return null;
     }
     
-    public static ListNode reverse(ListNode head) {
-    	ListNode curr = head;
-    	ListNode prev = null;
-    	ListNode next = null;
-    	ListNode newHead = null;
+    public static int length(ListNode head) {
+    	int i = 0;
     	
-    	while (curr != null) {
-    		next = curr.next;
-    		curr.next = prev;
-    		prev = curr;
-    		if (next == null) {
-    			newHead = curr;
-    		}
-    		curr = next;
-    	}
+		ListNode n = head;
+		while (n != null) {
+			n = n.next;
+			i++;
+		}
     	
-    	return newHead;
+    	return i;
     }
     
     public static void printList(ListNode head) {
@@ -56,7 +61,6 @@ public class IntersectionOfTwoLinkedLists {
     
 	public static void main(String[] args) {
 		ListNode headA = new ListNode(1);
-		ListNode node2 = new ListNode(2);
 		headA.next = new ListNode(2);
 		
 //		headA = reverse(headA);
@@ -64,7 +68,8 @@ public class IntersectionOfTwoLinkedLists {
 //		System.out.println();
 		
 		ListNode headB = new ListNode(0);
-		headB.next = new ListNode(3);
+		headB.next = new ListNode(-1);
+		headB.next.next = new ListNode(2);
 		
 		ListNode intersection = getIntersectionNode(headA, headB);
 		System.out.println(intersection != null ? intersection.val : null);
