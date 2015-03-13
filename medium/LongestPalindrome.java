326a5b0da662eaf0cab779de0418c0eda8ccbc4b
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-14 00:16:08 +0800

    LongestPalindrome

--- medium/LongestPalindrome.java
+++ medium/LongestPalindrome.java
@@ -1,14 +1,15 @@
 
+/**
+ * Sliding window
+ */
 public class LongestPalindrome {
 
     public static void longest_palind(String s) {
-        char[] a = s.toCharArray();
-        
-        for (int i = a.length - 1; i > 1; i--) { // length of window
-            for (int j = 0; j < a.length - i; j++) { // number of slides
-                if (isPalindrome(a, j, j+i)) {
+        for (int i = s.length() - 1; i > 1; i--) { // length of window
+            for (int j = 0; j < s.length() - i; j++) { // number of slides
+                if (isPalindrome(s, j, j+i)) {
                     for (int k = j; k <= i; k++) {
-                        System.out.print(a[k]);
+                        System.out.print(s.charAt(k));
                     }
                     System.out.println();
                     return;
@@ -17,11 +18,11 @@ public class LongestPalindrome {
         }
     }
     
-    public static boolean isPalindrome(char[] a, int i, int j) {
+    public static boolean isPalindrome(String s, int i, int j) {
         int m = i + (j-i)/2;
         
         for (int x = i; x <= m; x++) {
-            if (a[x] != a[j - x]) {
+            if (s.charAt(x) != s.charAt(j - x)) {
                 return false;
             }
         }
@@ -29,7 +30,7 @@ public class LongestPalindrome {
     }
     
 	public static void main(String[] args) {
-//		longest_palind("aba");//
+		longest_palind("abaa");//
 		longest_palind("abcdxyzyxabcdaaa");//xyzyx
 	}
 
