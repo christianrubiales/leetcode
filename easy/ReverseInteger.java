0dc76c2c27881f2cf23fe548bd2d057b7a133de7
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-17 01:35:23 +0800

    ReverseInteger

---
+++ easy/ReverseInteger.java
@@ -0,0 +1,24 @@
+
+public class ReverseInteger {
+
+    public static int reverse(int x) {
+        boolean negative = x < 0;
+        
+        if (negative) {
+        	x *= -1;
+        }
+        long l = Long.parseLong(new StringBuilder().append(x).reverse().toString().replace("-", ""));
+        x =  l > Integer.MAX_VALUE ? 0 : (int) l;
+        
+        return negative ? x * -1 : x;
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(reverse(123));//321
+		System.out.println(reverse(-123));//-321
+		System.out.println(reverse(1534236469));//9646324351
+		System.out.println(reverse(-2147483648));
+		System.out.println(reverse(1534236469));
+	}
+
+}
