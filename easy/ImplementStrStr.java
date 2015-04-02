5267590e9ff87d7fa19edeea7d502bfab22e7b14
Christian Rubiales <christianrubiales@yahoo.com>
2015-04-02 22:46:39 +0800

    ImplementStrStr

---
+++ easy/ImplementStrStr.java
@@ -0,0 +1,12 @@
+
+public class ImplementStrStr {
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+    
+	public static void main(String[] args) {
+
+	}
+
+}
