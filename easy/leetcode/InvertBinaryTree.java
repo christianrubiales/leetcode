93e64c9a4f78ae9de1c39d3d77f970167bd9d8b0
Christian Rubiales <christianrubiales@yahoo.com>
2015-10-11 23:08:27 +0800

    InvertBinaryTree

---
+++ easy/leetcode/InvertBinaryTree.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class InvertBinaryTree {
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+    public TreeNode invertTree(TreeNode root) {
+        if (root != null) {
+            swap(root);
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        
+        return root;
+    }
+    
+    public void swap(TreeNode node) {
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right= temp;
+    }
+    
+}
