13056b465a7f5d981db9917574787f70323f06c3
Christian Rubiales <christianrubiales@yahoo.com>
2015-10-20 20:20:47 +0800

    ReverseWords

---
+++ easy/ReverseWords.java
@@ -0,0 +1,21 @@
+
+public class ReverseWords {
+
+    public static String reverseWords(String s) {
+        String[] array = s.split("\\s+");
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = array.length - 1; i > -1; i--) {
+            sb.append(array[i]).append(" ");
+        }
+        
+        return sb.toString().trim();
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(reverseWords(""));
+		System.out.println(reverseWords("  a    b  "));
+		System.out.println(reverseWords("a b c"));
+	}
+
+}
