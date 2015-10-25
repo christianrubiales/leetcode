b2faa7a41e3f54dad9d09e08c7b109f51fdc7502
Christian Rubiales <christianrubiales@yahoo.com>
2015-10-25 23:33:54 +0800

    AddDigitsLinear

---
+++ easy/AddDigitsLinear.java
@@ -0,0 +1,13 @@
+
+public class AddDigitsLinear {
+
+    public static int addDigits(int num) {
+    	return (num - 1) % 9 + 1;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(addDigits(0));
+		System.out.println(addDigits(38));
+	}
+
+}
